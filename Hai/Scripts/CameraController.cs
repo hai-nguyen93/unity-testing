8db1da01379d9bd9e698c75d85a55204ac4feeb7
Hai Nguyen <54726078+hai-nguyen93@users.noreply.github.com>
2019-10-04 19:12:29 -0700

    Add files via upload

---
+++ Hai/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject targetToFocus;
+    public float offsetDistance = 2f;
+
+    void Start()
+    {
+
+    }
+
+
+    void LateUpdate()
+    {
+        float distance = Mathf.Abs(transform.position.x - targetToFocus.transform.position.x);       
+        if (distance >= offsetDistance)
+        {
+            Vector3 dest = new Vector3(targetToFocus.transform.position.x, transform.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, dest, Time.deltaTime);
+        }     
+    }
+}
